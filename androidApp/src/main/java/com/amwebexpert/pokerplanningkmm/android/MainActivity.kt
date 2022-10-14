fbcbd2365dfb3de2fbbfd468c7faa54a0300a940
André (BRP) <andre.masson@brp.com>
2022-10-14 16:34:46 -0400

    refactor: using compose & material design for the UI

--- androidApp/src/main/java/com/amwebexpert/pokerplanningkmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/amwebexpert/pokerplanningkmm/android/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.amwebexpert.pokerplanningkmm.android
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
@@ -11,40 +10,28 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.amwebexpert.pokerplanningkmm.Greeting
 
 import androidx.compose.runtime.*
-import androidx.lifecycle.Lifecycle
-import com.amwebexpert.pokerplanningkmm.service.PokerPlanningService
-import com.amwebexpert.pokerplanningkmm.ws.WebSocketService
-import com.amwebexpert.pokerplanningkmm.ws.WsTextMessageListener
+import androidx.lifecycle.ViewModelProvider
 import com.plcoding.kotlinflows.ui.theme.MyApplicationTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     companion object {
         private val TAG = MainActivity::class.java.simpleName
     }
 
-    private fun isActivityInForeground() = lifecycle.currentState.isAtLeast(Lifecycle.State.RESUMED)
-    private val webSocketService get() = WebSocketService.instance
-    private val pokerPlanningService get() = PokerPlanningService.instance
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         setContent {
             MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    val viewModel = viewModel<MainViewModel>()
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
                     val greetingsState = viewModel.greetingsFlow.collectAsState(initial = "Loading...")
+                    val incomingMessages = viewModel.incomingWebSocketMessages.collectAsState(initial = "N/A")
 
                     Column() {
                         Box() {
@@ -52,6 +39,9 @@ class MainActivity : ComponentActivity() {
                         }
                         Box() {
                             Text("Async result:result:\n ${greetingsState.value}")
+                        }
+                        Box() {
+                            Text("Async poker state:\n ${incomingMessages.value}")
                         }
                     }
                 }
@@ -61,65 +51,12 @@ class MainActivity : ComponentActivity() {
 
     override fun onResume() {
         super.onResume()
-        handleWebSocketsCommunications()
-    }
-
-    private fun handleWebSocketsCommunications() {
-        lifecycleScope.launch { // or GlobalScope.launch {
-            withContext(Dispatchers.IO) {
-                connectToWebSocket()
-            }
-        }
+        viewModel.handleWebSocketsCommunications()
     }
 
     override fun onPause() {
         super.onPause()
-        handleWebSocketsDisconnection()
-    }
-
-    private fun handleWebSocketsDisconnection() {
-        lifecycleScope.launch { // or GlobalScope.launch {
-            withContext(Dispatchers.IO) {
-                webSocketService.disconnect()
-            }
-        }
-    }
-
-    private suspend fun connectToWebSocket() {
-        webSocketService.connect(
-            hostname = "ws-poker-planning.herokuapp.com",
-            roomUUID = "e78caaee-a1a2-4298-860d-81d7752226ae",
-            listener = object : WsTextMessageListener {
-
-                override fun onConnectSuccess() {
-                    Log.i(TAG, "onConnectSuccess")
-                    runOnUiThread {
-                        //_binding?.btnJoinRoom?.isEnabled = true
-                    }
-                }
-
-                override fun onConnectFailed() {
-                    Log.w(TAG, "onConnectFailed, nothing to do because the onClose already handle reconnections")
-                }
-
-                override fun onClose() {
-                    Log.w(TAG, "onClose")
-                    runOnUiThread {
-                        // _binding?.btnJoinRoom?.isEnabled = false
-                    }
-
-                    if (isActivityInForeground()) {
-                        handleWebSocketsCommunications()
-                    }
-                }
-
-                override fun onMessage(text: String) {
-                    Log.i(TAG, "onMessage:\n\t $text")
-                    runOnUiThread {
-                        // val session = Json.decodeFromString<PokerPlanningSession>(text)//_binding?.textSocketResponse?.setText(session.toString())
-                    }
-                }
-            })
+        viewModel.handleWebSocketsDisconnection()
     }
 }
 
--- androidApp/src/main/java/com/amwebexpert/pokerplanningkmm/android/MainViewModel.kt
+++ androidApp/src/main/java/com/amwebexpert/pokerplanningkmm/android/MainViewModel.kt
@@ -1,11 +1,27 @@
 package com.amwebexpert.pokerplanningkmm.android
 
+import android.util.Log
 import com.amwebexpert.pokerplanningkmm.Greeting
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.amwebexpert.pokerplanningkmm.service.PokerPlanningService
+import com.amwebexpert.pokerplanningkmm.ws.WebSocketService
+import com.amwebexpert.pokerplanningkmm.ws.WsTextMessageListener
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainViewModel: ViewModel() {
+    companion object {
+        private val TAG = MainViewModel::class.java.simpleName
+    }
 
+    private val webSocketService get() = WebSocketService.instance
+    private val pokerPlanningService get() = PokerPlanningService.instance
+
+    val incomingWebSocketMessages = MutableSharedFlow<String>()
     val greetingsFlow = flow<String> {
         val currentValue = try {
             Greeting().greetingRemote()
@@ -14,5 +30,53 @@ class MainViewModel: ViewModel() {
         }
 
         emit(currentValue)
+    }
+
+    fun handleWebSocketsCommunications() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                connectToWebSocket()
+            }
+        }
+    }
+
+    fun handleWebSocketsDisconnection() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                webSocketService.disconnect()
+            }
+        }
+    }
+
+    private suspend fun connectToWebSocket() {
+        webSocketService.connect(
+            hostname = "ws-poker-planning.herokuapp.com",
+            roomUUID = "e78caaee-a1a2-4298-860d-81d7752226ae",
+            listener = object : WsTextMessageListener {
+                override fun onConnectSuccess() {
+                    Log.i(TAG, "onConnectSuccess")
+                }
+
+                override fun onConnectFailed() {
+                    Log.w(TAG, "onConnectFailed, nothing to do because the onClose already handle reconnections")
+                }
+
+                override fun onClose() {
+                    Log.w(TAG, "onClose")
+
+                    // TODO callback?
+//                    if (isActivityInForeground()) {
+//                        handleWebSocketsCommunications()
+//                    }
+                }
+
+                override fun onMessage(text: String) {
+                    Log.i(TAG, "onMessage:\n\t $text")
+
+                    viewModelScope.launch {
+                        incomingWebSocketMessages.emit(text)
+                    }
+                }
+            })
     }
 }
